5db3ff70cdaf22baea22fbd8f9e3ecbf8fc9ef21
liu-qinglin666 <1343797887@qq.com>
2022-11-23 16:14:29 +0800

    个人图书馆app完成

--- app/src/main/java/com/example/myapplication/InputBookItemActivity.java
+++ app/src/main/java/com/example/myapplication/InputBookItemActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class InputBookItemActivity extends AppCompatActivity {
 
@@ -25,6 +26,7 @@ public class InputBookItemActivity extends AppCompatActivity {
         String publisher=this.getIntent().getStringExtra("publisher");
         String putdate=this.getIntent().getStringExtra("putdate");
         String isbn=this.getIntent().getStringExtra("isbn");
+
 
         EditText editTextTitle=findViewById(R.id.edittext_shop_item_title);
         EditText editTextAuthor=findViewById(R.id.edittext_shop_item_title_price);
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -32,10 +34,13 @@ public class MainActivity extends AppCompatActivity {
     public static final int MENU_ID_ADD = 1;
     public static final int MENU_IN_UPDATE = 2;
     public static final int MENU_ID_DELETE = 3;
-    private ArrayList<BookItem> shopItems;
+    private ArrayList<BookItem> bookItems;
     private MainRecycleViewAdapter mainRecycleViewAdapter;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private int[] pictures={R.drawable.book1,R.drawable.book2,R.drawable.book3,R.drawable.book4,R.drawable.book5,
+                            R.drawable.book6,R.drawable.book7,R.drawable.book8,R.drawable.book9,R.drawable.book10,
+                            R.drawable.book11};
 
 
 
@@ -53,8 +58,8 @@ public class MainActivity extends AppCompatActivity {
                         String putdate=bundle.getString("putdate");
                         String isbn=bundle.getString("isbn");
                         int position=bundle.getInt("position");
-                        shopItems.add(position,new BookItem(title,author,year,translator,publisher,putdate,isbn,R.drawable.book2));
-                        new DataSaver().Save(this,shopItems);
+                        bookItems.add(position,new BookItem(title,author,year,translator,publisher,putdate,isbn,pictures[(int) (Math.random()*11)]));
+                        new DataSaver().Save(this, bookItems);
                         mainRecycleViewAdapter.notifyItemInserted(position);
                     }
 
@@ -74,9 +79,9 @@ public class MainActivity extends AppCompatActivity {
                         String putdate=bundle.getString("putdate");
                         String isbn=bundle.getString("isbn");
                         int position=bundle.getInt("position");
-                        shopItems.get(position).setTitle(title);
-                        shopItems.get(position).setAuthor(author);
-                        new DataSaver().Save(this,shopItems);
+                        bookItems.get(position).setTitle(title);
+                        bookItems.get(position).setAuthor(author);
+                        new DataSaver().Save(this, bookItems);
                         mainRecycleViewAdapter.notifyItemChanged(position);
                     }
 
@@ -100,6 +105,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button button_about=findViewById(R.id.about);
+        button_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent=new Intent();
+                intent.setClass(MainActivity.this,AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
         Button button_add=(Button)findViewById(R.id.add);
         button_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,89 +122,105 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent=new Intent(MainActivity.this, InputBookItemActivity.class);
                 intent.putExtra("position",0);
                 addDataLauncher.launch(intent);
+
             }
         });
 
 
-        RecyclerView recyclerViewMain=findViewById(R.id.recycleview);
+        RecyclerView recyclerViewMain=findViewById(R.id.recyclerview);
 
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerViewMain.setLayoutManager(linearLayoutManager);
 
         DataSaver dataSaver=new DataSaver();
-        shopItems=dataSaver.Load(this);
+        bookItems =dataSaver.Load(this);
 
-        if(shopItems.size()==0) {
-            shopItems.add(new BookItem("Android群英传","徐宣生 著,电子工业出版社","2015-11","某某人",
+        if(bookItems.size()==0) {
+            bookItems.add(new BookItem("Android群英传","徐宣生","2015-11","某某人",
                     "电子工业出版社","2015-11","21313",R.drawable.book5));
         }
-        mainRecycleViewAdapter= new MainRecycleViewAdapter(shopItems);
+        mainRecycleViewAdapter= new MainRecycleViewAdapter(bookItems);
         recyclerViewMain.setAdapter(mainRecycleViewAdapter);
 
     }
 
+
+
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId())
-        {
-            case MENU_ID_ADD:
-                Intent intent=new Intent(this, InputBookItemActivity.class);
-                intent.putExtra("position",item.getOrder());
-                addDataLauncher.launch(intent);
-                break;
-            case MENU_IN_UPDATE:
-                Intent intentUpdate=new Intent(this, InputBookItemActivity.class);
-                intentUpdate.putExtra("position",item.getOrder());
-                //intentUpdate.putExtra("title",shopItems.get(item.getOrder()).getTitle());
-                updateDataLauncher.launch(intentUpdate);
-                break;
-            case MENU_ID_DELETE:
-                AlertDialog alertDialog=new AlertDialog.Builder(this)
-                        .setTitle(R.string.confirmation)
-                        .setMessage(R.string.sure_to_delete)
-
-                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                shopItems.remove(item.getOrder());
-                                new DataSaver().Save(MainActivity.this,shopItems);
-                                mainRecycleViewAdapter.notifyItemRemoved(item.getOrder());
-
-                            }
-                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-
-                            }
-                        }).create();
-                alertDialog.show();
-                break;
-        }
+//        switch(item.getItemId())
+//        {
+//            case MENU_ID_ADD:
+//                Intent intent=new Intent(this, InputBookItemActivity.class);
+//                intent.putExtra("position",item.getOrder());
+//                addDataLauncher.launch(intent);
+//                break;
+//            case MENU_IN_UPDATE:
+//                Intent intentUpdate=new Intent(this, InputBookItemActivity.class);
+//                intentUpdate.putExtra("position",item.getOrder());
+//                //intentUpdate.putExtra("title",shopItems.get(item.getOrder()).getTitle());
+//                updateDataLauncher.launch(intentUpdate);
+//                break;
+//            case MENU_ID_DELETE:
+//                AlertDialog alertDialog=new AlertDialog.Builder(this)
+//                        .setTitle(R.string.confirmation)
+//                        .setMessage(R.string.sure_to_delete)
+//
+//                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                bookItems.remove(item.getOrder());
+//                                new DataSaver().Save(MainActivity.this, bookItems);
+//                                mainRecycleViewAdapter.notifyItemRemoved(item.getOrder());
+//
+//                            }
+//                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//
+//
+//                            }
+//                        }).create();
+//                alertDialog.show();
+//                break;
+//        }
         return super.onContextItemSelected(item);
     }
+
+
+
 
     public class MainRecycleViewAdapter extends RecyclerView.Adapter<MainRecycleViewAdapter.ViewHolder>{
 
         private ArrayList<BookItem> localDataSet;
+        private AdapterView.OnItemClickListener itemClickListener;
 
         public class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
             private final TextView textViewTitle;
 
-            public TextView getTextViewAuthor() {
-                return textViewAuthor;
-            }
+
 
             private final TextView textViewAuthor;
             private final ImageView imageViewImage;
             private final TextView textViewYear;
 
+            public final LinearLayout getLinearLayout_book() {
+                return linearLayout_book;
+            }
+
+            private LinearLayout linearLayout_book;
 
             private final TextView textViewTranslator;
             private final TextView textViewPublisher;
             private final TextView textViewPutdate;
             private final TextView textViewIsbn;
+
+
+
+            public TextView getTextViewAuthor() {
+                return textViewAuthor;
+            }
 
             public TextView getTextViewTranslator() {
                 return textViewTranslator;
@@ -225,6 +256,7 @@ public class MainActivity extends AppCompatActivity {
                 textViewPublisher=view.findViewById(R.id.edittext_shop_item_title_publisher);
                 textViewPutdate=view.findViewById(R.id.edittext_shop_item_title_put_date);
                 textViewIsbn=view.findViewById(R.id.edittext_shop_item_title_isbn);
+                linearLayout_book=view.findViewById(R.id.linearLayout);
 
                 view.setOnCreateContextMenuListener(this);
             }
@@ -239,9 +271,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
-                contextMenu.add(0,MENU_ID_ADD,getAdapterPosition(),"Add"+getAdapterPosition());
-                contextMenu.add(0,MENU_IN_UPDATE,getAdapterPosition(),"Update"+getAdapterPosition());
-                contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete"+getAdapterPosition());
+//                contextMenu.add(0,MENU_ID_ADD,getAdapterPosition(),"Add"+getAdapterPosition());
+//                contextMenu.add(0,MENU_IN_UPDATE,getAdapterPosition(),"Update"+getAdapterPosition());
+//                contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete"+getAdapterPosition());
 
             }
         }
@@ -265,11 +297,54 @@ public class MainActivity extends AppCompatActivity {
             viewHolder.getImageViewImage().setImageResource(localDataSet.get(position).getResourceId());
 
 
-        }
+            viewHolder.getLinearLayout_book().setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    AlertDialog alertDialog=new AlertDialog.Builder(MainActivity.this)
+                            .setTitle(R.string.confirmation)
+                            .setMessage(R.string.sure_to_delete)
 
+                            .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    bookItems.remove(viewHolder.getAdapterPosition());
+                                    new DataSaver().Save(MainActivity.this, bookItems);
+                                    mainRecycleViewAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+
+                                }
+                            }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                                }
+                            }).create();
+                    alertDialog.show();
+                    return false;
+                }
+            });
+
+            viewHolder.getLinearLayout_book().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position=viewHolder.getAdapterPosition();
+                    Intent intent=new Intent(MainActivity.this,InputBookItemActivity.class);
+                    intent.putExtra("position",viewHolder.getAdapterPosition());
+                    intent.putExtra("title",bookItems.get(position).getTitle());
+                    intent.putExtra("author",bookItems.get(position).getAuthor());
+                    intent.putExtra("translator",bookItems.get(position).getTranslator());
+                    intent.putExtra("publisher",bookItems.get(position).getPublisher());
+                    intent.putExtra("putdate",bookItems.get(position).getPutdate());
+                    intent.putExtra("isbn",bookItems.get(position).getIsbn());
+                    updateDataLauncher.launch(intent);
+                }
+            });
+        }
         @Override
         public int getItemCount() {
             return localDataSet.size();
         }
+
+
     }
 }
